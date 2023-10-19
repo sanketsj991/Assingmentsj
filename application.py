e2bcaeded32c25157e3073fb178c5adcb07d027e
Ubuntu <sanketjadhav199@gmail.com>
2023-10-19 05:16:47 -0700

    fixedcode

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
-application = Flask(__name__)
+
+application = Flask(__name)
 
 @application.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    application .run(debug=True, host='0.0.0.0')
-
+    application.run(debug=True, host='0.0.0.0')
