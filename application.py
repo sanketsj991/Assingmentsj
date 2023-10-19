6a6b8f65db26df4223a2295f1165025c0188d162
Ubuntu <sanketjadhav199@gmail.com>
2023-10-19 05:19:03 -0700

    fixedcode

--- application.py
+++ application.py
@@ -7,4 +7,7 @@ def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0')
+    application.run(
+        debug=True,
+        host='0.0.0.0'
+    )
