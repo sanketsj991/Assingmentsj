c177ae1ae9dc5bdadc853cdc63c72722ba27df2c
Ubuntu <sanketjadhav199@gmail.com>
2023-10-19 05:37:14 -0700

    fixedcode

--- application.py
+++ application.py
@@ -1,21 +1,9 @@
-"""
-Your module docstring goes here (replace this line).
-"""
-
 from flask import Flask
+   application = Flask(__name__)
 
-application = Flask(__name)
+   @application.route('/')
+   def hello():
+       return "Hello, World!"
 
-@application.route('/')
-def hello() -> str:
-    """
-    This is the docstring for the hello function.
-    It returns a greeting message.
-    """
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    application.run(
-        debug=True,
-        host='0.0.0.0'
-    )
+   if __name__ == '__main__':
+       application.run(debug=True, host='0.0.0.0')
